81841934c958623711bd6ecd784d80d3f47f7192
yeahyeah <wangye0523@users.noreply.github.com>
2016-10-07 02:17:37 -0500

    Add files via upload

---
+++ pre_process.py
@@ -0,0 +1,78 @@
+#!/usr/lib/env python
+#-*- coding=utf-8 -*-
+
+import jieba
+import jieba.analyse
+import xlrd
+import numpy as np
+import json
+import os
+import re
+import codecs
+jieba.analyse.set_stop_words("stopwords.txt")
+
+path= os.getcwd()
+files=os.listdir(path)
+xls_file=[]
+for file in files:
+    if file.endswith('.xls'):
+        xls_file.append(file)
+#         print file
+# s=re.findall(file,files)
+
+label_com = []
+#
+#
+# exit()
+for xls in xls_file:
+    label_test = re.match('(.+)_(.+).xls',xls)
+
+    # print type(xls),(label_test.group(1)),label_test.group(2)
+    label_1 = label_test.group(1).decode('utf-8')
+    label_2 = label_test.group(2).decode('utf-8')
+
+    data  = xlrd.open_workbook(xls)
+    table = data.sheet_by_name(u'公司公告')
+    # ncols = table.ncols
+    # nrows = table.nrows
+    s=table.col_values(2)
+    s=s[1:-2]
+    # colname = table.row_value(2)
+    i=0
+    for ss in s:
+        ss= [ss,label_1,label_2]
+        label_com.append(ss)
+
+label_com=label_com[0:60000]
+
+
+with codecs.open('label.json', 'w', encoding='utf-8') as fff1:
+    json.dump(label_com,fff1,indent=4, ensure_ascii=False)
+
+
+all_sentence=[]
+corpus= []
+first_label=[]
+second_label = []
+# f=f[0:5]
+for ff in label_com:
+    # print ff[0]
+    # seg_list =[]
+    seg_list = jieba.lcut(ff[0], cut_all=False)  # Use Precise mode to cut the word
+    all_sentence.append(','.join(seg_list))
+    first_label.append(ff[1])
+    second_label.append(ff[2])
+
+
+
+
+import pickle
+
+with open('data.pickle', 'w') as fff:
+    # Pickle the 'data' dictionary using the highest protocol available.
+    pickle.dump(all_sentence, fff, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(first_label, fff, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(second_label, fff, pickle.HIGHEST_PROTOCOL)
+
+
+print '****'*40
---
+++ process.py
@@ -0,0 +1,102 @@
+#!/usr/lib/env python
+#-*- coding=utf-8 -*-
+
+import jieba
+import jieba.analyse
+import numpy as np
+import pickle
+jieba.analyse.set_stop_words("stopwords.txt")
+
+
+with open('data.pickle', 'rb') as fff:
+    # Pickle the 'data' dictionary using the highest protocol available.
+    all_sentence= np.array(pickle.load(fff))
+    first_label = pickle.load(fff)
+    second_label= pickle.load(fff)
+
+
+
+from sklearn import preprocessing
+label_preprocess_1  = preprocessing.LabelEncoder()
+first_array         = label_preprocess_1.fit(first_label)
+##################
+
+label_preprocess_2  = preprocessing.LabelEncoder()
+second_array        = label_preprocess_2.fit(second_label)
+
+########
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+####
+vectorizer = CountVectorizer()
+all_sentence_tmp = vectorizer.fit_transform(all_sentence)
+all_sentence_vec = all_sentence_tmp.toarray()
+
+
+# transformer = TfidfTransformer()
+# tfidf       = transformer.fit_transform(vectorizer.fit_transform(all_sentence_vec))
+
+from sklearn import cross_validation
+
+
+
+print '*****'*4
+
+from sklearn.naive_bayes import MultinomialNB
+#
+
+clf = MultinomialNB(alpha=0.01)
+scores1 = cross_validation.cross_val_score(clf, all_sentence_vec, first_label, cv=5)
+scores2 = cross_validation.cross_val_score(clf, all_sentence_vec, second_label, cv=5)
+
+
+print scores1,scores2
+exit()
+
+
+
+#
+# from sklearn.pipeline import Pipeline
+# #nbc means naive bayes classifier
+# nbc_1 = Pipeline([
+#     ('vect', CountVectorizer()),
+#     ('clf', MultinomialNB()),
+# ])
+# nbc_2 = Pipeline([
+#     ('vect', HashingVectorizer(non_negative=True)),
+#     ('clf', MultinomialNB()),
+# ])
+# nbc_3 = Pipeline([
+#     ('vect', TfidfVectorizer()),
+#     ('clf', MultinomialNB()),
+# ])
+#
+# nbcs = [nbc_1, nbc_2, nbc_3]
+#
+#
+#
+#
+# from sklearn import cross_validation
+#
+# X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(all_sentence_vec, first_label_array, test_size=0.3, random_state=0)
+#
+# # from sklearn import cross_val_score, KFold
+# from scipy.stats import sem
+#
+# def evaluate_cross_validation(clf, X, y, K):
+#     # create a k-fold croos validation iterator of k=5 folds
+#     cv = cross_validation.KFold(len(y), K, shuffle=True, random_state=0)
+#     # by default the score used is the one returned by score method of the estimator (accuracy)
+#     scores = cross_validation.cross_val_score(clf, X, y, cv=cv)
+#     print scores
+#     print ("Mean score: {0:.3f} (+/-{1:.3f})").format(
+#         np.mean(scores), sem(scores))
+#
+# for nbc in nbcs:
+#     evaluate_cross_validation(nbc, X_train, Y_train, 5)
+#
+
+
+print '*****'*4
